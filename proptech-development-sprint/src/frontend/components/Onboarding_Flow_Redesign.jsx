c43d69692646a93d4ad493d4a3cece42a2b3b860
Aryan-juneja <aryanjuneja011@gmail.com>
2025-05-04 14:20:34 +0530

    CU-86cyukxzy - Task 4.1: Onboarding Flow Redesign

---
+++ proptech-development-sprint/src/frontend/components/Onboarding_Flow_Redesign.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Onboarding_Flow_Redesign = () => {
+  return (
+    <View>
+      <Text>Onboarding_Flow_Redesign</Text>
+    </View>
+  )
+}
+
+export default Onboarding_Flow_Redesign
+
+const styles = StyleSheet.create({})
